44d3e376ee4edd549db28ad97a9dc8f6208f9b46
Mehranlip <realmehranasadi@gamil.com>
2023-05-06 19:48:08 +0430

    setup redux

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
-    console.log('Movies');
+    const { data } = useGetMoviesQuery(); //fetching data from an API
+    console.log(data);
+
     return (
         <div>Movies</div>
-    )
+    );
 }
 
-export default Movies
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
+import store from "./app/store";
 
 const theme = createTheme({})
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>
     , document.getElementById("root"))
