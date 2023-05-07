c722437145561a55aab07bf1a24397a933c94e7c
Mehranlip <realmehranasadi@gamil.com>
2023-05-07 19:05:31 +0430

    fix middelware and end movies show

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { tmdbApi } from "../services/TMDB";
 
 
 export default configureStore({
     reducer: {
         [tmdbApi.reducerPath]: tmdbApi.reducer,
-    }
+    },
+
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(tmdbApi.middleware)
 })
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
+
 
 import App from "./components/App";
 import store from "./app/store";
 
 const theme = createTheme({})
 
-ReactDOM.render(
-    <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ThemeProvider>
-    </Provider>
-    , document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>
+);
+
