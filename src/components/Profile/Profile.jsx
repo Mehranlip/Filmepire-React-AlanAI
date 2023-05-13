b35e6903afd8556d492903bc0e273d4ec8e6a4b9
Mehranlip <realmehranasadi@gamil.com>
2023-05-13 19:56:13 +0430

    end logout functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Typography, Button, Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../features/auth';
+import { ExitToApp } from '@mui/icons-material';
 
 function Profile() {
-    console.log("Profile");
+    const { user } = useSelector(userSelector)
+
+    const favoriteMovies = []
+    const logout = () => {
+        localStorage.clear()
+
+        window.location.href = '/'
+    }
     return (
-        <div>Profile</div>
+        <Box>
+            <Box display="flex" justifyContent="space-between">
+                <Typography variant="h4" gutterBottom>My Profile</Typography>
+                <Button color="inherit" onClick={logout}>
+                    Logout &nbsp; <ExitToApp />
+                </Button>
+            </Box>
+            {!favoriteMovies.length ?
+                <Typography variant='h5'>
+                    Add favorites or watchlist some movies to see them here !
+                </Typography>
+                :
+                <Box>
+                    FAVORITE MOVIES
+                </Box>
+            }
+        </Box>
     )
 }
 
