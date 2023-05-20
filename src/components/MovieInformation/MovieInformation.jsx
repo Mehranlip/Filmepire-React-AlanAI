46ca5c9150702738170e1b7d3d954b6f7882e405
Mehranlip <realmehranasadi@gmail.com>
2023-05-20 18:38:43 +0430

    end side 2 information page

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material';
-import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack, Tune } from '@mui/icons-material';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
@@ -16,6 +16,9 @@ import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 function MovieInformation() {
     const dispatch = useDispatch()
     const classes = useStyles();
+
+    const isMovieFavorited = false
+    const isMovieWatchlisted = false
 
 
     const { id } = useParams()
@@ -38,6 +41,9 @@ function MovieInformation() {
             </Box>
         );
     }
+
+    const addToFavorites = () => { }
+    const addToWatchlist = () => { }
     return (
         <Grid container className={classes.containerSpaceAround}>
             <Grid item sm={12} lg={4}>
@@ -53,7 +59,7 @@ function MovieInformation() {
                 <Grid item className={classes.containerSpaceAround}>
                     <Box display="flex" align="center">
                         <Rating readOnly value={data.vote_average / 2} />
-                        <Typography variant='subtitle1' gutterBottom style={{ marginLeft: "10px" }}>
+                        <Typography variant="subtitle1" gutterBottom style={{ marginLeft: '10px' }}>
                             {data?.vote_average} / 10
                         </Typography>
                     </Box>
@@ -65,11 +71,69 @@ function MovieInformation() {
                     {data?.genres?.map((genre) => (
                         <Link key={genre.name} className={classes.links} to='/' onClick={() => dispatch(selectGenreOrCategory(genre.id))}>
                             <img src={genreCategoryIcons[genre?.name.toLowerCase()]} className={classes.genreImage} height={30} />
-                            <Typography color="textPrimary" variant="subtitle1">
+                            <Typography color="textPrimary" variant="subtitle1" >
                                 {genre?.name}
                             </Typography>
                         </Link>
                     ))}
+                </Grid>
+                <Typography variant='h5' gutterBottom style={{ marginTop: "10px" }} >
+                    Overview
+                </Typography>
+                <Typography style={{ marginBottom: "2rem" }}>
+                    {data?.overview}
+                </Typography>
+                <Typography variant='h5' gutterBottom>
+                    Top Cast
+                </Typography>
+                <Grid item container spacing={2}>
+                    {data && data.credits.cast.map((character, i) => (
+                        character.profile_path && (
+                            <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character?.id}`} style={{ textDecoration: 'none' }}>
+                                <img
+                                    className={classes.castImage}
+                                    src={`https://image.tmdb.org/t/p/w500/${character?.profile_path}`}
+                                    alt={character?.name}
+                                />
+                                <Typography color="textPrimary">{character?.name}</Typography>
+                                <Typography color="textSecondary">{character?.character.split('/')[0]}</Typography>
+                            </Grid>
+                        )
+                    )).slice(0, 6)}
+                </Grid>
+
+                <Grid item container style={{ marginTop: '2rem' }} justifyContent="center">
+                    <div className={classes.buttonsContainer}>
+                        <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+                            <ButtonGroup size="medium" variant="outlined">
+                                <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language />}>Website</Button>
+                                <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon />}>IMDB</Button>
+                                <Button onClick={() => { }} endIcon={<Theaters />}>Trailer</Button>
+                            </ButtonGroup>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+                            <ButtonGroup size="medium" variant="outlined">
+                                <Button onClick={addToFavorites} endIcon={isMovieFavorited ? <FavoriteBorderOutlined /> : <Favorite />}>
+                                    {isMovieFavorited ? 'Unfavorite' : 'Favorite'}
+                                </Button>
+                                <Button onClick={addToWatchlist} endIcon={isMovieWatchlisted ? <Remove /> : <PlusOne />}>
+                                    Watchlist
+                                </Button>
+                                <Button endIcon={<ArrowBack />} sx={{ borderColor: 'primary.main' }} onClick={() => history(-1)}>
+                                    <Typography style={{ textDecoration: 'none' }} color="inherit" variant="subtitle2">
+                                        Back
+                                    </Typography>
+                                </Button>
+                            </ButtonGroup>
+                        </Grid>
+
+
+
+
+
+
+                    </div>
                 </Grid>
             </Grid>
         </Grid >
