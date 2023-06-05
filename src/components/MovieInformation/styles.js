171d56bb62b28f510481fa3934134fd8f5c7f602
Mehran <realmehranasadi@gmail.com>
2023-06-05 12:50:24 +0330

    fix poster

--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -14,8 +14,9 @@ export default makeStyles((theme) => ({
     poster: {
         borderRadius: '20px',
         boxShadow: '0.5em 1em 1em rgb(64, 64, 70)',
-        width: '80%',
-        height: '100%',
+        width: '100%',
+        height: '90%',
+
         [theme.breakpoints.down('xl')]: {
             margin: '0 auto',
             width: '100%',
